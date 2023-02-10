9ebcd3209eea13e1fd1d65dcb6faea14d88551a3
Rodrigo Silos <88406401+r-silos@users.noreply.github.com>
2023-02-09 19:30:40 -0600

    edited gameboard class and made test case for gameboard

--- gameboard.py
+++ gameboard.py
@@ -1,15 +1,33 @@
 class GameBoard:
-    def __init__(self):
-        pass
+    def __init__(self, game_dimension):
+        self.board = GameBoard.make_gameboard(game_dimension)
 
-    # function that allows to make gameboard to spec
+    # function takes in a dimension i and returns an ixi 2d array
     def make_gameboard(self, dimensions):
         mainboard = []
         for i in range(dimensions, 0, -1):
             temp = []
             start_letter = 'a'
-            for i in range(dimensions):
-                temp.append("{}{}".format(start_letter, i + 1))
+            for j in range(dimensions, 0, -1):
+                current_label = "{}{}".format(start_letter, i)
+                temp.append(GameBoard(j, i, current_label))
                 start_letter = chr(ord(start_letter) + 1)
             mainboard.append(temp)
         return mainboard
+
+# class representing each square on gameboard
+
+
+class BoardSquare:
+    def __init__(self, x_pos, y_pos, label, color="white"):
+        self.x = x_pos
+        self.y = y_pos
+        self.color = color
+        self.label = label
+        self.color = color
+
+    def get_x_position(self):
+        return self.x
+
+    def get_y_position(self):
+        return self.y
---
+++ test_gameboard.py
@@ -0,0 +1,13 @@
+import unittest
+import gameboard
+
+
+class TestGameBoard(unittest.TestCase):
+
+    # Qnd function just to check if able to run test
+    def test_test_able_to_run(self):
+        self.assertEqual(10, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
