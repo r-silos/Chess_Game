9237df3206a0af23fa69c03e10ae14e52f759d81
Rodrigo Silos <88406401+r-silos@users.noreply.github.com>
2023-02-17 15:54:32 -0600

    Expended test cases

--- gameboard.py
+++ gameboard.py
@@ -3,19 +3,25 @@ class GameBoard:
         self.board = GameBoard.make_gameboard(game_dimension)
 
     # function takes in a dimension i and returns an ixi 2d array
-    def make_gameboard(self, dimensions):
+    def make_gameboard(dimensions):
         mainboard = []
-        for i in range(dimensions, 0, -1):
+        # making double change
+        for i in range(dimensions - 1, -1, -1):
             temp = []
             start_letter = 'a'
-            for j in range(dimensions, 0, -1):
+            for j in range(dimensions - 1, -1, -1):
                 current_label = "{}{}".format(start_letter, i)
-                temp.append(GameBoard(j, i, current_label))
+                print("the current x is {} and y is {}".format(j, i))
+                temp.append(BoardSquare(j, i, current_label))
                 start_letter = chr(ord(start_letter) + 1)
             mainboard.append(temp)
+            print("New Row!")
         return mainboard
 
-# class representing each square on gameboard
+    # iterate through gameboard and print out internal labels
+    def display_gameboard(self):
+        pass
+        # class representing each square on gameboard
 
 
 class BoardSquare:
@@ -23,8 +29,9 @@ class BoardSquare:
         self.x = x_pos
         self.y = y_pos
         self.color = color
-        self.label = label
-        self.color = color
+        self.internal_label = label
+        # external label will be what square displays at its position
+        self.external_label = ""
 
     def get_x_position(self):
         return self.x
--- test_gameboard.py
+++ test_gameboard.py
@@ -1,12 +1,57 @@
 import unittest
-import gameboard
+from gameboard import GameBoard
+# unit test video: https://www.youtube.com/watch?time_continue=763&v=6tNS--WetLI&embeds_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Drun%2Bunit%2Btests%2Bpython%26rlz%3D1C1ONGR_enUS976US976%26sxsrf%3DAJOqlzUWca9VYEDJSSuw8Gkgw8CYLJma6w%3A16766630&feature=emb_logo
 
 
 class TestGameBoard(unittest.TestCase):
 
     # Qnd function just to check if able to run test
-    def test_test_able_to_run(self):
+    def test_able_to_run(self):
         self.assertEqual(10, 10)
+
+    def test_first_added_gameboard_position(self):
+        board_dim = 8
+        set = GameBoard(board_dim)
+        self.assertEqual(set.board[0][0].x, (board_dim - 1))
+        self.assertEqual(set.board[0][0].y, (board_dim - 1))
+        self.assertEqual(set.board[0][0].internal_label, "a7")
+        self.assertEqual(set.board[0][0].color, "white")
+
+    def test_second_added_gameboard_position(self):
+        board_dim = 8
+        set = GameBoard(board_dim)
+        self.assertEqual(set.board[0][1].x, (board_dim - 2))
+        self.assertEqual(set.board[0][1].y, (board_dim - 1))
+        self.assertEqual(set.board[0][1].internal_label, "b7")
+        self.assertEqual(set.board[0][0].color, "white")
+
+    def test_2ndlast_added_gameboard_position(self):
+        board_dim = 8
+        set = GameBoard(board_dim)
+        self.assertEqual(set.board[board_dim - 1][board_dim - 2].x, 1)
+        self.assertEqual(set.board[board_dim - 1][board_dim - 2].y, 0)
+        self.assertEqual(set.board[board_dim - 1]
+                         [board_dim - 2].internal_label, "g0")
+        self.assertEqual(set.board[board_dim - 1]
+                         [board_dim - 2].color, "white")
+
+    def test_last_added_gameboard_position(self):
+        board_dim = 8
+        set = GameBoard(board_dim)
+        self.assertEqual(set.board[board_dim - 1][board_dim - 1].x, 0)
+        self.assertEqual(set.board[board_dim - 1][board_dim - 1].y, 0)
+        self.assertEqual(set.board[board_dim - 1]
+                         [board_dim - 1].internal_label, "h0")
+        self.assertEqual(set.board[board_dim - 1]
+                         [board_dim - 1].color, "white")
+
+    def test_middle_postion_added(self):
+        board_dim = 8
+        set = GameBoard(board_dim)
+        self.assertEqual(set.board[4][4].x, 3)
+        self.assertEqual(set.board[4][4].y, 3)
+        self.assertEqual(set.board[4][4].internal_label, "e3")
+        self.assertEqual(set.board[4][4].color, "white")
 
 
 if __name__ == '__main__':
